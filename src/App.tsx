cf2faa236868e302157c045ef93a7ef3cf36be3a
Naza Gomez <nazasanchez12.26@gmail.com>
2024-07-10 20:39:25 -0600

    Añadir archivo no rastreado

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { Route,Routes } from 'react-router-dom';
+import Navbar from '../src/componets/home/Navbar'
 
 function App() {
 
   return (
     <>
+    <Navbar/>
+      <div>
+    <Routes>        
+      <Route path="/login"  element={<Login />} />
+      <Route path="/books"  element={<Books />}/>
+      <Route path="/rooms"  element={<Rooms/>}/>
+      <Route path="/technoteams"  element={<Technoteams/>}/>
+      <Route path="/courses"  element={<Courses/>}/>
+      <Route path="/events"  element={<Events/>}/>
+      <Route path="/libraryfriends"  element={<Libraryfriends/>}/>
+
+    </Routes>
+    </div>
     </>
   )
 }
---
+++ src/componets/home/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div className='navBar'>
+            <div className='login'>
+                <Link to="/login">Iniciar Sesión</Link>
+            </div>
+            <div className='books'>
+                <Link to="/books">Libros</Link>
+            </div>
+            <div className='room'>
+                <Link to="/rooms">Salas</Link>
+            </div>
+            <div className='technoteams'>
+                <Link to="/technoteams">Equipo Tecnológico</Link>
+            </div>
+            <div className='courses'>
+                <Link to="/courses">Cursos</Link>
+            </div>
+            <div className='events'>
+                <Link to="/events">Eventos</Link>
+            </div>
+            <div className='libraryfriends'>
+                <Link to="/libraryfriends">Amiguitos de la biblioteca</Link>
+            </div>
+        </div>
+    )
+
+}
+
+
+export default Navbar;
