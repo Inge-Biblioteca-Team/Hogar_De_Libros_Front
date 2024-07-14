a4a7b8a7c8c5e6d61a37548b186df94347025445
Dian <dian7875.a@gmail.com>
2024-07-14 13:27:33 -0600

    Revert "intento"
    This reverts commit c669d12a6b5cbc64396a0a75464e9fb1cfd393ed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import MyArtists from "./pages/MyArtista";
-import MyContact from "./pages/MyContact";
-import Rating from "./pages/Rating";
 
-const App = () => {
+function App() {
+
   return (
-    <div className="App" >
-    <MyArtists/>
-    <MyContact/>
-    <Rating/>
-    </div>
-  );
-};
+    <>
+    </>
+  )
+}
 
-export default App;
+export default App
--- src/interfaces/index.tsx
+++
@@ -1,7 +0,0 @@
-//intefaces para enviar el mensaje de contacto
-export interface MessageContact{
-    Name: string,
-    Email:string,
-    Subject:string,
-    Message:string
-}
