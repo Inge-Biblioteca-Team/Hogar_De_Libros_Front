5442c2525aaa054036f1e3131c71cb2632729ae0
Akion <akion.cheng.jimenez@est.una.ac.cr>
2024-08-09 11:14:44 -0600

    Finalizacion de carrusel terminado para homePage de Books

--- src/features/Books/Pages/BooksHomePage.tsx
+++ src/features/Books/Pages/BooksHomePage.tsx
@@ -7,19 +7,19 @@ const BooksHomePage = () => {
   return (
     <main className="w-full text-center text-2xl font-bold flex flex-col gap-5">
       <section>
-        <h2 >Libros mas populares</h2>
+        <h2 className="mb-4" >Libros mas populares</h2>
         <ListMostPopularBooks />
       </section>
       <section>
-        <h2>Libros De Regalo</h2>
+        <h2 className="mb-4">Libros De Regalo</h2>
         <ListFreeBoosks />
       </section>
       <section>
-        <h2>Libros de reserva</h2>
+        <h2 className="mb-4">Libros de reserva</h2>
         <ListReserveBooks/>
       </section>
       <section>
-        <h2>Nuevos Ingresos</h2>
+        <h2 className="mb-4">Nuevos Ingresos</h2>
         <ListNewRevenue/>
       </section>
     </main>
--- src/features/Books/screens/ListMostPopularBooks.tsx
+++ src/features/Books/screens/ListMostPopularBooks.tsx
@@ -1,8 +1,7 @@
 import { useQuery } from "react-query";
 import { GetBooks } from "../services/SvBooks";
 import { Book } from "../type/Book";
-import { useState } from "react";
-import BookCard from "../components/BookCard";
+import CarrouselOfImages from "../components/CarrouselOfImages";
 
 const ListMostPopularBooks = () => {
   const {
@@ -11,55 +10,13 @@ const ListMostPopularBooks = () => {
     isLoading,
   } = useQuery<Book[], Error>(["PopBooks"], GetBooks);
 
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? books.length - 15 : prevIndex - 1
-    );
-  };
-
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === books.length - 15 ? 0 : prevIndex + 1
-    );
-  };
-
   if (isLoading) return <span>Loading...</span>;
   if (error) return <span>Error: {error.message}</span>;
   return (
-    <section
-    className="relative px-4 w-full max-sm:w-4/5"
-    id="Programs"
-  >
-    <div className="flex items-center gap-1 justify-between">
-      <button
-        type="button"
-        onClick={prevSlide}
-        className="bg-gray-300 rounded-full p-2 max-sm:hidden"
-      >
-        &lt;
-      </button>
-      <div className="w-full overflow-hidden max-sm:overflow-x-scroll">
-        <article
-          className="flex transition-transform duration-300 gap-2 h-96 w-52 "
-          style={{ transform: `translateX(-${currentIndex * 200}%)` }}
-        >
-          {books.map((books, index) => (
-            <BookCard key={index} Book={books} />
-          ))}
-        </article>
-      </div>
-      <button
-        type="button"
-        onClick={nextSlide}
-        className="bg-gray-300 rounded-full p-2 max-sm:hidden"
-      >
-        &gt;
-      </button>
-    </div>
-  </section>
-  )
-}
+    <section>
+      <CarrouselOfImages ListBooks={books} />
+    </section>
+  );
+};
 
-export default ListMostPopularBooks
+export default ListMostPopularBooks;
