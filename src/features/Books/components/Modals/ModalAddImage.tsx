a53430d0302931fca074646e889813c6734d0653
Dian <dian7875.a@gmail.com>
2024-09-08 11:52:31 -0600

    Carga de imagenes al servidor

--- src/features/Books/components/Modals/ModalAddImage.tsx
+++ src/features/Books/components/Modals/ModalAddImage.tsx
@@ -1,7 +1,7 @@
-import { Modal, TextInput, Button, Spinner } from "flowbite-react";
+import { Modal, TextInput, Button, Spinner, FileInput } from "flowbite-react";
 import { useState } from "react";
-import searchImages from "../../services/SvSearchIMG";
 import { IMGsearh } from "../../type/SearchIMG";
+import { searchImages, uploadImage } from "../../services/SvSearchIMG";
 
 const ModalAddImage = ({
   showModal,
@@ -14,17 +14,46 @@ const ModalAddImage = ({
 }) => {
   const [searchIMG, setSearchIMG] = useState<string>("");
   const [images, setImages] = useState<IMGsearh[]>([]);
-
-  const [Loading, setLoading] = useState(false);
-  const [NoResult, setNoResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [noResult, setNoResult] = useState(false);
+  const [localImage, setLocalImage] = useState<string | null>(null);
+  const [file, setFile] = useState<File | null>(null);
 
   const handleSearch = async () => {
     setLoading(true);
     setNoResult(false);
-    const results = await searchImages(searchIMG);
-    results.length == 0 ? setNoResult(true) : setImages(results);
+    try {
+      const results = await searchImages(searchIMG);
+      results.length === 0 ? setNoResult(true) : setImages(results);
+    } catch (error) {
+      console.error('Error al buscar imágenes:', error);
+      setNoResult(true);
+    }
     setLoading(false);
   };
+
+  const handleLocalImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const uploadedFile = e.target.files?.[0];
+    if (uploadedFile) {
+      setFile(uploadedFile);
+      const imageURL = URL.createObjectURL(uploadedFile);
+      setLocalImage(imageURL);
+    }
+  };
+
+  const handleConfirmLocalImage = async () => {
+    if (file) {
+      try {
+        const imageUrl = await uploadImage(file);
+        onImageSelect(imageUrl);
+        setLocalImage(null);
+        setFile(null);
+      } catch (error) {
+        console.error('Error uploading image:', error);
+      }
+    }
+  };
+
 
   return (
     <Modal show={showModal} onClose={onCloseModal} popup>
@@ -37,7 +66,7 @@ const ModalAddImage = ({
           onChange={(e) => setSearchIMG(e.target.value)}
         />
         <Button className="mt-3" color={"blue"} onClick={handleSearch}>
-          {Loading ? (
+          {loading ? (
             <span>
               <Spinner aria-label="Spinner button example" size="sm" color={"purple"} />
               <span className="pl-3">Cargando</span>
@@ -46,8 +75,8 @@ const ModalAddImage = ({
             "Buscar"
           )}
         </Button>
-        {NoResult ? (
-          "No hay resultados para el titulo buscado"
+        {noResult ? (
+          "No hay resultados para el título buscado"
         ) : (
           <div className="grid grid-cols-3 gap-4 mt-4">
             {images.map((image) => (
@@ -61,6 +90,28 @@ const ModalAddImage = ({
             ))}
           </div>
         )}
+        <div className="mt-6">
+          <p className="mb-2 font-semibold">O cargar una imagen local</p>
+          <FileInput onChange={handleLocalImageUpload} />
+
+          {localImage && (
+            <div className="mt-4">
+              <p className="mb-2 font-semibold">Previsualización:</p>
+              <img
+                src={localImage}
+                alt="Previsualización de la imagen local"
+                className="rounded shadow-md w-full h-auto"
+              />
+              <Button
+                className="mt-3"
+                color={"blue"}
+                onClick={handleConfirmLocalImage}
+              >
+                Confirmar Imagen Local
+              </Button>
+            </div>
+          )}
+        </div>
       </Modal.Body>
     </Modal>
   );
