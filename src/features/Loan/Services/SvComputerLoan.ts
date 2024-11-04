1e66e042390891fa66ad3c790fe2db1ecb92fca2
Dian <dian7875.a@gmail.com>
2024-11-04 15:38:06 -0600

    Correccion de prestamo de equipo de computo

--- src/features/Loan/Services/SvComputerLoan.ts
+++ src/features/Loan/Services/SvComputerLoan.ts
@@ -1,27 +1,33 @@
 import api from "../../../Services/AxiosConfig";
 import { NewWSLoan, NewWSMantenance } from "../Types/ComputerLoan";
 
-
-
 //Gets
 const GetStatus = async () => {
   try {
     const response = await api.get(`work-stations/workstation/Status`);
     return response.data;
   } catch (error) {
-    console.error("Error al cargar el estado de los equipos de cómputo:", error);
+    console.error(
+      "Error al cargar el estado de los equipos de cómputo:",
+      error
+    );
     throw error;
   }
 };
 
-const GetWSLoans = async (page: number, limit: number, StartDate?: string, MachineNumber?:string) => {
+const GetWSLoans = async (
+  page: number,
+  limit: number,
+  StartDate?: string,
+  MachineNumber?: string
+) => {
   try {
     const response = await api.get(`computer-loan`, {
       params: {
         Page: page,
         Limit: limit,
-        ...(StartDate && { StartDate }), 
-        ...(MachineNumber && {MachineNumber})
+        ...(StartDate && { StartDate }),
+        ...(MachineNumber && { MachineNumber }),
       },
     });
     return response.data;
@@ -31,22 +37,27 @@ const GetWSLoans = async (page: number, limit: number, StartDate?: string, Machi
   }
 };
 
-
-const FinalizeLoan = async (NMachine: number) => {
+const FinalizeLoan = async (MachineNumber: number) => {
   try {
-    const response = await api.patch(`computer-loan/finish/${NMachine}`);
+    const response = await api.patch(`computer-loan`,{MachineNumber:MachineNumber});
     return response.data;
   } catch (error) {
-    console.error("Error al finalizar el préstamo del equipo de cómputo:", error);
+    console.error(
+      "Error al finalizar el préstamo del equipo de cómputo:",
+      error
+    );
     throw error;
   }
 };
 const ReactiveWS = async (NMachine: number) => {
   try {
-    const response = await api.patch(`computers/${NMachine}/reactive`);
+    const response = await api.patch(`work-stations/${NMachine}/available`);
     return response.data;
   } catch (error) {
-    console.error("Error al reactivar como disponible el equipo de cómputo:", error);
+    console.error(
+      "Error al reactivar como disponible el equipo de cómputo:",
+      error
+    );
     throw error;
   }
 };
@@ -54,12 +65,15 @@ const ReactiveWS = async (NMachine: number) => {
 const MantenanceWS = async (Data: NewWSMantenance) => {
   try {
     const response = await api.patch(
-      `computers/${Data.machineNumber}/maintenance`,
+      `work-stations/${Data.MachineNumber}/maintenance`,
       Data
     );
     return response.data;
   } catch (error) {
-    console.error("Error al cambiar el estado a mantenimiento al equipo de cómputo:", error);
+    console.error(
+      "Error al cambiar el estado a mantenimiento al equipo de cómputo:",
+      error
+    );
     throw error;
   }
 };
