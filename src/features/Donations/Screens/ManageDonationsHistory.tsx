77a96d0b21cbbcb6943e77ec812377472b218b82
Akion <akion.cheng.jimenez@est.una.ac.cr>
2024-11-16 18:31:57 -0600

    Responsive de amigos

--- src/features/Donations/Screens/ManageDonationsHistory.tsx
+++ src/features/Donations/Screens/ManageDonationsHistory.tsx
@@ -38,7 +38,7 @@ const ManageDonationsHistory = () => {
   return (
     <>
       <DonationsCrumbs text="Donaciones finalizadas" />
-      <main className="flex flex-col items-center justify-center w-full gap-5">
+      <main className="overflow-x-hidden flex flex-col items-center justify-center w-full gap-5">
         <section className=" max-sm:flex-col flex w-4/5 gap-2">
           <div>
             <Label value="Categoría del colaborador" />
