2e786a19f92b966c7d8937f920754f50fe2ffce8
Obando-26 <166901255+Obando-26@users.noreply.github.com>
2024-11-17 16:51:17 -0600

    cambio en la tabla para que se adapte mas a todo los dispositivos

--- src/features/Users/Pages/ManageUsers.tsx
+++ src/features/Users/Pages/ManageUsers.tsx
@@ -54,7 +54,7 @@ const ManageUsers = () => {
         <BreadLastItems text="Gestión de usuarios" />
       </BreadCrumbsItems>
       <div className="overflow-x-hidden flex place-content-center">
-        <div className="w-4/5 ">
+        <div className="w-4/5 flex flex-col items-center justify-center pt-1">
           <div className="w-full sm:w-full flex justify-center max-sm:pb-8">
           <SearchUsers
             setYear={setYear}
