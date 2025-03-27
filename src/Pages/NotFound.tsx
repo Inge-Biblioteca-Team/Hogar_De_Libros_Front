8acac3c4ee22155a452a7c613ba5e95ab0aee427
Dian <dian7875.a@gmail.com>
2025-03-27 17:02:11 -0600

    Not found page y mision vision

---
+++ src/Pages/NotFound.tsx
@@ -0,0 +1,32 @@
+import { Button } from "flowbite-react";
+import { Link } from "react-router-dom";
+import notFound from "../assets/404.svg";
+
+const NotFound = () => {
+  return (
+    <>
+      <div className="w-full h-[100vh] flex items-center justify-center">
+        <div className="text-center space-y-4 flex items-center justify-center flex-col">
+          <img src={notFound} alt="No encontrado" />
+          <h1 className="text-4xl font-bold text-gray-800">
+            ¡Ups! Página no encontrada
+          </h1>
+          <p className="text-lg text-gray-600">
+            No pudimos encontrar la página que buscas.
+          </p>
+          <p className="text-lg text-gray-800">
+            Pero no te preocupes, siempre puedes explorar nuestros recursos
+          </p>
+          <Button color={"blue"} type="button" title="Regresar al Inicio">
+            <Link
+              to="/">
+              Regresar al Inicio
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
