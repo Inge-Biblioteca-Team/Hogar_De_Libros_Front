f1046fd9df536b9708ee818c2de4a3902f4940b4
Dian <dian7875.a@gmail.com>
2025-04-15 14:45:18 -0600

    Label Fix

--- src/components/ModalFooters.tsx
+++ src/components/ModalFooters.tsx
@@ -4,7 +4,8 @@ const ModalFooters = ({ onClose, isLoading }: { onClose: () => void, isLoading: 
   return (
     <>
       <Modal.Footer className="dark:bg-[#2d2d2d] flex bg-white lg:bg-transparent items-center justify-center gap-3">
-        <Button title="Cancelar y regresar" tabIndex={2} color="red" onClick={onClose} disabled={isLoading}>
+        <Button
+         title="Cancelar y regresar" tabIndex={2} color="red" onClick={onClose} disabled={isLoading}>
           Cancelar
         </Button>
         <Button title="Confirmar" color="blue" type="submit" disabled={isLoading}>
